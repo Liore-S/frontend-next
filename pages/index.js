2527ec264672b93d254edfaed896226f26d8439b
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-02-02 16:53:45 +0700

    remove href

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,17 @@ export default function Home() {
         </h1>
       </div>
       <div className='grid grid-cols-2 grid-rows-2 w-fit gap-2'>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Home</a></Link> 
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Youtube</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Profile</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>About</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Home</a></Link> 
+        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Youtube</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Profile</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >About</a></Link>
         <br />
       </div>
       <div className='flex'>
-        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' href='/'>Home</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' href='/'>Youtube</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' href='/'>Profile</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' href='/'>About</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Home</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Youtube</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Profile</a></Link>
+        <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >About</a></Link>
 
       </div>
       {/* <button className="relative m-3 p-3 bg-slate-200 hover:bg-slate-300 rounded-md text-black align-middle shadow-lg 
