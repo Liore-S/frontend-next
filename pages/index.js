c77bee58179765ba91bd00b5cc44b16bfd84ccbb
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-02-02 16:43:04 +0700

    underline animation

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,28 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='h1-blue'>
-      Hello Manuk
-    </h1>
+    <body >
+      <div className='align-middle'>
+        <h1 className='h1-blue mt-4 hover:animate-ping '>
+          Hello Liore
+        </h1>
+      </div>
+      <div className='grid grid-cols-2 grid-rows-2 w-fit gap-2'>
+        <a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Home</a>
+        <a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Youtube</a>
+        <a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>Profile</a>
+        <a className='underline-nav relative text-center mx-3 px-0 py-1' href='/'>About</a>
+        <br />
+      </div>
+      <div className='flex'>
+        <a className='underline-nav relative text-center m-3 p-1' href='/'>Home</a>
+        <a className='underline-nav relative text-center m-3 p-1' href='/'>Youtube</a>
+        <a className='underline-nav relative text-center m-3 p-1' href='/'>Profile</a>
+        <a className='underline-nav relative text-center m-3 p-1' href='/'>About</a>
+
+      </div>
+      {/* <button className="relative m-3 p-3 bg-slate-200 hover:bg-slate-300 rounded-md text-black align-middle shadow-lg 
+        after:absolute after:w-[100%] after:h-1 after:left-0 after:bottom-0 after:bg-orange-600">Button A</button> */}
+    </body>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  darkMode:'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '0.7':'0.175rem'
+      }
+    },
   },
   plugins: [],
 }
