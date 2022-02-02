95f428a87539153e98fa13927876adb88ff36be1
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-02-02 19:54:05 +0700

    logo & cleanup

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,27 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <body >
-      <div className='align-middle'>
+      {/* <div className='align-middle'>
         <h1 className='h1-blue mt-4 hover:animate-ping '>
           Hello Liore
         </h1>
-      </div>
-      <div className='grid grid-cols-2 grid-rows-2 w-fit gap-2'>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Home</a></Link> 
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Youtube</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Profile</a></Link>
-        <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >About</a></Link>
-        <br />
-      </div>
+      </div> */}
       <div className='flex'>
+        <Image src="/images/SB.jpg" alt="Logo" width={150} height={150} priority></Image>
+        <div className='grid grid-cols-2 grid-rows-2 w-fit h-fit gap-2 mt-7'>
+          <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Home</a></Link>
+          <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Youtube</a></Link>
+          <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >Profile</a></Link>
+          <Link href={'/'}><a className='underline-nav relative text-center mx-3 px-0 py-1' >About</a></Link>
+          <br />
+        </div>
+      </div>
+      {/* <div className='flex'>
         <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Home</a></Link>
         <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Youtube</a></Link>
         <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >Profile</a></Link>
         <Link href={'/'}><a className='underline-nav relative text-center m-3 p-1' >About</a></Link>
-
-      </div>
+      </div> */}
       {/* <button className="relative m-3 p-3 bg-slate-200 hover:bg-slate-300 rounded-md text-black align-middle shadow-lg 
         after:absolute after:w-[100%] after:h-1 after:left-0 after:bottom-0 after:bg-orange-600">Button A</button> */}
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif','system-ui','Monsterrat','sans-serif'],
+    },
     extend: {
       spacing:{
         '0.7':'0.175rem'
