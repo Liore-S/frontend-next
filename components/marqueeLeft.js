151dfa3f36b344db78a77bad582409f63af2918b
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-02-03 18:10:11 +0700

    marquee fix

---
+++ components/marqueeLeft.js
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function SlidingLeft() {
+    return (
+        <>
+            <div className="marquee-item">
+                <Link href={"/manuk"} >
+                    <Image src="/images/anime_scenery.jpg" width={320} height={190} className='w-5/6'></Image>
+                </Link>
+            </div>
+            <div className="marquee-item">
+                <Link href={'/manuk'} className="marquee-item">
+                    <Image src="/images/Ayame-dorime.jpg" width={320} height={190} className='w-5/6 '></Image>
+                </Link >
+            </div>
+            <div className="marquee-item">
+                <Link href={'/manuk'} className="marquee-item">
+                    <Image src="/images/chroneco_Oozora_Subaru.jpg" width={320} height={190} className='w-5/6 '></Image>
+                </Link >
+            </div>
+            <div className="marquee-item">
+                <Link href={'/manuk'} className="marquee-item">
+                    <Image src="/images/towa.jpg" width={320} height={190} className='w-5/6 '></Image>
+                </Link >
+            </div>
+            <div className="marquee-item">
+                <Link href={'/manuk'} className="marquee-item" >
+                    <Image src="/images/subaru_rex.jpg" width={320} height={190} className='w-5/6 '></Image>
+                </Link>
+            </div>
+        </>
+    )
+}
