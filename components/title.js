b1194473a850e36b3ee92d9ce42872be4e4e1f04
Ilham Ilyas Rasyid <goober395@gmail.com>
2022-02-05 09:12:09 +0700

    responsive title

--- components/title.js
+++ components/title.js
@@ -1,9 +1,9 @@
 export default function Title(){
     return (
         <>
-            <div className="relative h-32 z-10">
-                <h1 className="absolute font-black text-7xl bg-white px-2 w-fit text-right right-7 rounded-sm">A NEW MAGICAL</h1>
-                <h1 className="absolute font-black text-7xl bg-white px-2 w-fit text-right right-7 top-16 rounded-sm">EXPREIENCE</h1>
+            <div className="relative h-32 z-10 ">
+                <h1 className="absolute font-black md:text-7xl text-4xl bg-white px-2 w-fit text-right right-7 top-16 md:top-0 rounded-sm">A NEW MAGICAL</h1>
+                <h1 className="absolute font-black md:text-7xl text-4xl bg-white px-2 w-fit text-right right-7 top-28 md:top-16 rounded-sm">EXPREIENCE</h1>
             </div>
         </>
     )
